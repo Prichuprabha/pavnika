1679cc22943580bd6d9ee3f964bfe070e42cbcc7
Prichuprabha <prichu.p@gmail.com>
2026-08-25 16:18:25 +0400

    Admin: edit saree BB004

--- products-data.js
+++ products-data.js
@@ -1929,7 +1929,7 @@ window.PRODUCTS = [
     "material": "Semi Kanchipuram",
     "shade": "Pink",
     "price": 349,
-    "sold": true,
+    "sold": false,
     "images": [
       "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/OIO1eNJhmc3xSwkV1I6h/pub/gme2ZYmAFHCdeO3J6jmD.jpg",
       "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/OIO1eNJhmc3xSwkV1I6h/pub/ELXiy7n1gPqnDdmUoSZe.jpg",
