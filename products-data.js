d4485817b3ef5a5155bb059e2f2a5ed42f308d45
Prichuprabha <prichu.p@gmail.com>
2026-09-09 23:47:15 +0400

    Admin: bulk update via CSV (115 sarees)

--- products-data.js
+++ products-data.js
@@ -1166,7 +1166,7 @@ window.PRODUCTS = [
       "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/OIO1eNJhmc3xSwkV1I6h/pub/pWlbikzb5iBPkvs1kuOX.jpg"
     ],
     "image": "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/OIO1eNJhmc3xSwkV1I6h/pub/N8eDpaDJhn6ie6fE1Hpd.jpg",
-    "salePrice": 315
+    "salePrice": null
   },
   {
     "id": "VW013",
