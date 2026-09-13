df0cf8542507c65ce6986dae1b4c00a8c3930b15
Prichuprabha <prichu.p@gmail.com>
2026-09-13 11:40:16 +0400

    Admin: edit saree BB008

--- products-data.js
+++ products-data.js
@@ -2589,7 +2589,7 @@ window.PRODUCTS = [
     "design": "Traditional",
     "material": "Semi Kanchipuram",
     "shade": "White / Off-white",
-    "price": 375,
+    "price": 370,
     "sold": false,
     "images": [
       "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/OIO1eNJhmc3xSwkV1I6h/pub/zGVD7IFXYbWE3mCdFGgL.jpg",
